601d712cab039f887ed906a4568461832d930cd5
Hasan Ali <ihasanalihasan@gmail.com>
2025-04-04 23:27:33 +0300

    refactor: make it work on all cases

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,5 @@
 import utils.*
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
 
 fun main() {
 
@@ -10,61 +7,71 @@ fun main() {
     check(
         name = VALID_3x3,
         correctResult = true,
-        result = isSudokuValid(TestCases.sudoku3x3)
+        result = isSudokuValid(sudoku3x3)
     )
 
     check(
         name = VALID_4x4,
         correctResult = true,
-        result = isSudokuValid(TestCases.sudoku4x4)
+        result = isSudokuValid(sudoku4x4)
+    )
+    check(
+        name = VALID_4x4_1,
+        correctResult = true,
+        result = isSudokuValid(sudoku4x4_1)
     )
 
     check(
         name = VALID_8x8,
         correctResult = true,
-        result = isSudokuValid(TestCases.sudoku8x8)
+        result = isSudokuValid(sudoku8x8)
     )
 
     check(
         name = VALID_16x16,
         correctResult = true,
-        result = isSudokuValid(TestCases.sudoku16x16)
+        result = isSudokuValid(sudoku16x16)
     )
+
     check(
         name = INVALID_4x4,
         correctResult = false,
-        result = isSudokuValid(TestCases.invalid4x4)
+        result = isSudokuValid(invalid4x4)
     )
     check(
-        name = EMPTY_LIST,
-        correctResult = false,
-        result = isSudokuValid(emptyArray())
+        name = EMPTY,
+        correctResult = true,
+        result = isSudokuValid(empty)
     )
+
+    check(
+        name = EMPTY_LIST,
+        correctResult = true,
+        result = isSudokuValid(empty)
+    )
+
     check(
         name = INVALID_8x8,
         correctResult = false,
-        result = isSudokuValid(TestCases.invalid8x8)
+        result = isSudokuValid(invalid8x8)
     )
+
     check(
         name = INVALID_16x16,
         correctResult = false,
-        result = isSudokuValid(TestCases.invalid16x16)
+        result = isSudokuValid(invalid16x16)
     )
+
     check(
         name = INVALID_3x4,
         correctResult = false,
-        result = isSudokuValid(TestCases.invalid3x4)
+        result = isSudokuValid(invalid3x4)
     )
+
     check(
         name = INVALID_8x7,
         correctResult = false,
-        result = isSudokuValid(TestCases.invalid8x7)
+        result = isSudokuValid(invalid8x7)
     )
 }
-
-fun isSudokuValid(board: Array<IntArray>): Boolean{
-    return false
-}
-
-
 
